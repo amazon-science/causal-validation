6aa3d9efc08737bd3621f3c2ca7ead56f236b983
Semih Akbayrak <sakbyrk@amazon.com>
2024-09-17 14:00:23 +0000

    Formatting

--- src/causal_validation/transforms/__init__.py
+++ src/causal_validation/transforms/__init__.py
@@ -1,5 +1,5 @@
+from causal_validation.transforms.noise import Noise
 from causal_validation.transforms.periodic import Periodic
 from causal_validation.transforms.trends import Trend
-from causal_validation.transforms.noise import Noise
 
 __all__ = ["Trend", "Periodic", "Noise"]
--- tests/test_causal_validation/test_transforms/test_noise.py
+++ tests/test_causal_validation/test_transforms/test_noise.py
@@ -10,7 +10,10 @@ from causal_validation.testing import (
     TestConstants,
     simulate_data,
 )
-from causal_validation.transforms import Noise, Trend
+from causal_validation.transforms import (
+    Noise,
+    Trend,
+)
 from causal_validation.transforms.parameter import TimeVaryingParameter
 
 CONSTANTS = TestConstants()
@@ -18,16 +21,22 @@ DEFAULT_SEED = 123
 GLOBAL_MEAN = 20
 STATES = [42, 123]
 
+
 def test_slot_type():
     noise_transform = Noise()
     assert isinstance(noise_transform.noise_dist, TimeVaryingParameter)
 
-@given(loc=st.floats(min_value=-5., max_value=5.), 
-       scale=st.floats(min_value=.1, max_value=1.))
+
+@given(
+    loc=st.floats(min_value=-5.0, max_value=5.0),
+    scale=st.floats(min_value=0.1, max_value=1.0),
+)
 @settings(max_examples=5)
 def test_base_transform(loc: float, scale: float):
     base_data = simulate_data(GLOBAL_MEAN, DEFAULT_SEED)
-    noise_transform = Noise(noise_dist = TimeVaryingParameter(sampling_dist=norm(loc, scale)))
+    noise_transform = Noise(
+        noise_dist=TimeVaryingParameter(sampling_dist=norm(loc, scale))
+    )
     noisy_data = noise_transform(base_data)
 
     assert np.all(noisy_data.Xtr == base_data.Xtr)
@@ -35,9 +44,12 @@ def test_base_transform(loc: float, scale: float):
     assert np.all(noisy_data.ytr != base_data.ytr)
     assert np.all(noisy_data.yte != base_data.yte)
 
-@given(degree=st.integers(min_value=1, max_value=3), 
-       coefficient=st.floats(min_value=-1., max_value=1.), 
-       intercept=st.floats(min_value=-1., max_value=1.))
+
+@given(
+    degree=st.integers(min_value=1, max_value=3),
+    coefficient=st.floats(min_value=-1.0, max_value=1.0),
+    intercept=st.floats(min_value=-1.0, max_value=1.0),
+)
 @settings(max_examples=5)
 def test_composite_transform(degree: int, coefficient: float, intercept: float):
     trend_transform = Trend(degree=degree, coefficient=coefficient, intercept=intercept)
